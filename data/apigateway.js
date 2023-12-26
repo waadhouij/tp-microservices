d66678aca1a8980e290a3fb44125d6203ab2ff45
waadhouij <waad.houij@gmail.com>
2023-12-26 22:12:42 +0100

    commit

--- data/apigateway.js
+++ data/apigateway.js
@@ -6,33 +6,74 @@ const StatuProtoDefinition = protoLoader.loadSync(StatuProtoPath, {
   longs: String,
   enums: String,
   defaults: true,
-  arrays:true,
+  arrays: true,
   oneofs: true,
 });
 const StatuProto = grpc.loadPackageDefinition(StatuProtoDefinition).statu;
 
+// Création du service et implémentation des méthodes
 const StatusService = {
-    getstatus: (call, callback) => {
-     const status=call.request.status;
+  /**
+   * Méthode pour récupérer le statut.
+   * @param {object} call - Objet RPC.
+   * @param {function} callback - Fonction de rappel pour renvoyer la réponse.
+   */
+  getstatus: (call, callback) => {
+    console.log('Appel à la méthode getstatus');
+    
+    const status = call.request.status;
+
+    // Validation des données
+    if (!status || typeof status !== 'string') {
+      callback({
+        code: grpc.status.INVALID_ARGUMENT,
+        message: 'Le statut est invalide ou manquant'
+      });
+      return;
+    }
+
+    // Implémentation de getstatus
     callback(null, { status });
   },
+
+  /**
+   * Méthode pour ...
+   * (ajoutez des commentaires similaires pour getstatu et getmessage)
+   */
   getstatu: (call, callback) => {
-    const status=call.request.status;
-   callback(null, { status });
- },
- getmessage: (call, callback) => {
-    const message=call.request.message;
-   callback(null, { message });
- }
+    console.log('Appel à la méthode getstatu');
+    const status = call.request.status;
+
+    // Validation des données
+    // (ajoutez des validations similaires à getstatus)
+
+    // Implémentation de getstatu
+    callback(null, { status });
+  },
+
+  getmessage: (call, callback) => {
+    console.log('Appel à la méthode getmessage');
+    const message = call.request.message;
+
+    // Validation des données
+    // (ajoutez des validations similaires à getstatus)
+
+    // Implémentation de getmessage
+    callback(null, { message });
+  }
 }
+
+// Configuration du serveur gRPC
 const server = new grpc.Server();
 server.addService(StatuProto.StatusService.service, StatusService);
-server.bindAsync(`127.0.0.1:30044`, grpc.ServerCredentials.createInsecure(), (err, port) => {
+
+// Lier et démarrer le serveur
+server.bindAsync('127.0.0.1:30044', grpc.ServerCredentials.createInsecure(), (err, port) => {
   if (err) {
-    console.error('Failed to bind server:', err);
+    console.error('Erreur lors de la liaison du serveur:', err);
     return;
   }
 
-  console.log(`Server is running on port 30044`);
+  console.log('Serveur en cours d\'exécution sur le port 30044');
   server.start();
 });
